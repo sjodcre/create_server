0e5bf7fff7be1307a8674ea88b396e56ff746fe7
Samuel Omamalin <samuelomamalin@gmail.com>
2019-03-27 07:08:45 -0400

    connect frontend backend

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database ={
 	users:[
@@ -39,7 +41,18 @@ app.get('/',(req,res)=>{
 });
 
 app.post('/signin',(req,res) =>{
-
+	// bcrypt.compare("bacon", hash, function(err, res) {
+ //    // res == true
+	// });
+	// bcrypt.compare("veggies", hash, function(err, res) {
+	//     // res = false
+	// });
+	if (req.body.email ===database.users[0].email && 
+		req.body.password=== database.users[0].password) {
+		res.json(database.users[0]);
+	} else {
+		res.status(400).json('error logging in');
+	}
 });
 
 app.post('/register',(req, res) =>{
@@ -86,6 +99,6 @@ app.put('/image', (req, res) =>{
 	}
 })
 
-app.listen(3000, () =>{
-	console.log('app is running on port 3000');
+app.listen(3001, () =>{
+	console.log('app is running on port 3001');
 })
