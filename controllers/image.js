f49d9a1c5a22459be19d6c96d15b3d32981f0b52
Samuel Omamalin <samuelomamalin@gmail.com>
2019-03-29 03:40:20 -0400

    minor fixes

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '2a4557c268a74374a3c3f700e4c106bd'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+    .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+    .then(data => {
+    	res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with api'))
+}
+	
+
+const handleImage = (req, res, db) =>{
+	const {id} = req.body;
+	// same as db.select('*').from('users').where({id})
+	db('users').where('id', '=', id)
+	.increment('entries',1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0])
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+	module.exports = {
+		handleImage,
+		handleApiCall
+	}
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -14,9 +19,9 @@ const db = knex({
   }
 });
 
-db.select('*').from('users').then(data => {
-	// console.log(data);
-});
+// db.select('*').from('users').then(data => {
+// 	// console.log(data);
+// });
 
 const app = express();
 
@@ -55,80 +60,19 @@ app.get('/',(req,res)=>{
 	res.send(database.users);
 });
 
-app.post('/signin',(req,res) =>{
-	db.select('email','hash').from('login')
-	.where('email','=',req.body.email)
-	.then(data =>{
-		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-		if (isValid){
-			db.select('*').from('users')
-			.where('email','=',req.body.email)
-			.then(user =>{
-				res.json(user[0])
-			})
-			.catch(err=> res.status(400).json('unable to get user'))
-		} else {
-			res.status(400).json('wrong credentials inner')
-		}
-	})
-	.catch(err=> res.status(400).json('wrong credentials outer'))
-});
+app.post('/signin',signin.handleSignin(db, bcrypt));
 
-app.post('/register',(req, res) =>{
-	const { email, name, password } = req.body;	
-	const hash = bcrypt.hashSync(password);
-	db.transaction(trx =>{
-		trx.insert({hash, email})
-		.into('login')
-		.returning('email')
-		.then(loginEmail =>{
-			return trx('users')
-			.returning('*')
-			.insert({
-				email:loginEmail[0],
-				name: name,
-				joined : new Date(),
-			})
-			.then(user =>{
-				res.json(user[0]);
-			})
-		})
-		.then(trx.commit)
-		.catch(trx.rollback)
-	})	
-	.catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register',(req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
-app.get('/profile/:id', (req, res) => {
-	const {id } = req.params;
-	// same as db('users').where('id', '=', id)
-	db.select('*').from('users').where({id})
-	.then(user =>{
-		if(user.length){
-			res.json(user[0])	
-		} else {
-			res.status(400).json('user not found')
-		}
-		
-	})
-	.catch(err => res.status(400).json('error getting user'))
-	// if (!found) {
-	// 	res.status(400).json('not found');
-	// }
-})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
-app.put('/image', (req, res) =>{
-	const {id} = req.body;
-	// same as db.select('*').from('users').where({id})
-	db('users').where('id', '=', id)
-	.increment('entries',1)
-	.returning('entries')
-	.then(entries => {
-		res.json(entries[0])
-	})
-	.catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
-app.listen(3001, () =>{
-	console.log('app is running on port 3001');
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () =>{
+	console.log(`app is running on port ${PORT}`);
+	console.log(process.env.PORT);
 })
