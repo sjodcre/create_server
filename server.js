1c03e5e9015ccd227172db31f820f7279e4c154b
Samuel Omamalin <samuelomamalin@gmail.com>
2019-05-14 03:51:50 -0400

    add usage for website

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const fs = require('fs');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -23,6 +24,7 @@ const db = knex({
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -40,6 +42,19 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+app.post('/submitform', (req,res) => {
+	console.log(req.body);
+	fs.appendFile('./messages.txt', req.body.name + '\t' + req.body['email-address'] + '\t' 
+		+ req.body['contact-number'] + '\t' + req.body.subject + '\t' + req.body.message + '\n', err => {
+		if (err) {
+			console.log(err)
+		}
+	} );
+
+
+	res.send('okay');
+})
 
 
 const PORT = process.env.PORT
